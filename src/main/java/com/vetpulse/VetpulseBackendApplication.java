f7f9157d2c73228b859f63b89fdd8e29b4423ecc
Vítor Passeri <vitorpskaluf@hotmail.com>
2026-06-24 22:11:59 -0300

    feat: push inicial com estrutura do projeto

---
+++ src/main/java/com/vetpulse/VetpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vetpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetpulseBackendApplication.class, args);
+	}
+
+}
