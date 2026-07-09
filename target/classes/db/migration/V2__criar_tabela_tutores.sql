c39e4c4a3f9914d753302c6eec5cfb7c3e204a45
Vítor Passeri <vitorpskaluf@hotmail.com>
2026-07-08 22:56:36 -0300

    Delete target directory

--- target/classes/db/migration/V2__criar_tabela_tutores.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tutores (
-    id UUID NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-
-    CONSTRAINT pk_tutores PRIMARY KEY (id),
-    CONSTRAINT fk_tutores_usuarios FOREIGN KEY (id)
-        REFERENCES usuarios (id)
-        ON DELETE CASCADE
-);
